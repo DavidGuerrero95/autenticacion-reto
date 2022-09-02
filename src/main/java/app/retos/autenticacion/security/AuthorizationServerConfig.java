2cdaa39df70f20371b80ac8cd6f1b9e61fa540eb
santiago <davids.guerrero@udea.edu.co>
2022-09-02 04:10:35 -0500

    Cambios

--- src/main/java/app/retos/autenticacion/security/AuthorizationServerConfig.java
+++ src/main/java/app/retos/autenticacion/security/AuthorizationServerConfig.java
@@ -1,9 +1,6 @@
 package app.retos.autenticacion.security;
 
-import java.util.Base64;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,45 +15,47 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Base64;
+
 @RefreshScope
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-	@Autowired
-	private AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.tokenKeyAccess("permiteAll()").checkTokenAccess("isAuthenticated()");
-	}
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permiteAll()").checkTokenAccess("isAuthenticated()");
+    }
 
-	@Override
-	@PostMapping("/login/autorizacion/token")
-	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("flutter").secret(passwordEncoder.encode("udea")).scopes("read", "write")
-				.authorizedGrantTypes("password", "refresh_token");
-	}
+    @Override
+    @PostMapping("/login/autorizacion/token")
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("flutter").secret(passwordEncoder.encode("udea")).scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token");
+    }
 
-	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore())
-				.accessTokenConverter(accesTokenConverter());
-	}
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore())
+                .accessTokenConverter(accesTokenConverter());
+    }
 
-	@Bean
-	public JwtTokenStore tokenStore() {
-		return new JwtTokenStore(accesTokenConverter());
-	}
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accesTokenConverter());
+    }
 
-	@Bean
-	public JwtAccessTokenConverter accesTokenConverter() {
-		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey(Base64.getEncoder().encodeToString("codigo_secreto_ok_123456789".getBytes()));
-		return tokenConverter;
-	}
+    @Bean
+    public JwtAccessTokenConverter accesTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(Base64.getEncoder().encodeToString("codigo_secreto_ok_123456789".getBytes()));
+        return tokenConverter;
+    }
 
 }
